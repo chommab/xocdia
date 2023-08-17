f9b7dd4dc56b3372c4b9738020fadb0f64ca2bda
chommab <136581572+chommab@users.noreply.github.com>
2023-08-17 16:02:19 +0800

    Add files via upload

---
+++ src/settings.c9733.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/gameLoading.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"69b9a",main:"64ad5"}};
